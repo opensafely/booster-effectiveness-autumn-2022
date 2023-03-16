a0533929d923ade872b98e6ff67acb25bb4d5635
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2023-03-16 11:26:52 +0000

    update codelists

---
+++ analysis/codelists.py
@@ -0,0 +1,448 @@
+from cohortextractor import (codelist, codelist_from_csv, combine_codelists)
+
+
+covid_icd10 = codelist_from_csv(
+    "codelists/opensafely-covid-identification.csv",
+    system="icd10",
+    column="icd10_code",
+)
+
+ethnicity_codes_6 = codelist_from_csv(
+    "codelists/opensafely-ethnicity-snomed-0removed.csv",
+    system="snomed",
+    column="snomedcode",
+    category_column="Grouping_6",
+)
+
+## PRIMIS
+
+# Patients in long-stay nursing and residential care
+carehome = codelist_from_csv(
+    "codelists/primis-covid19-vacc-uptake-longres.csv", 
+    system="snomed", 
+    column="code",
+)
+
+# High Risk from COVID-19 code
+shield = codelist_from_csv(
+    "codelists/primis-covid19-vacc-uptake-shield.csv",
+    system="snomed",
+    column="code",
+)
+
+# Lower Risk from COVID-19 codes
+nonshield = codelist_from_csv(
+    "codelists/primis-covid19-vacc-uptake-nonshield.csv",
+    system="snomed",
+    column="code",
+)
+
+# Asthma Diagnosis code
+ast = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-ast.csv",
+  system="snomed",
+  column="code",
+)
+
+# Asthma Admission codes
+astadm = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-astadm.csv",
+  system="snomed",
+  column="code",
+)
+
+# Asthma systemic steroid prescription codes
+astrx = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-astrx.csv",
+  system="snomed",
+  column="code",
+)
+
+# Chronic Respiratory Disease
+resp_cov = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-resp_cov.csv",
+  system="snomed",
+  column="code",
+)
+
+# Chronic heart disease codes
+chd_cov = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-chd_cov.csv",
+  system="snomed",
+  column="code",
+)
+
+# Chronic kidney disease diagnostic codes
+ckd_cov = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-ckd_cov.csv",
+  system="snomed",
+  column="code",
+)
+
+# Chronic kidney disease codes - all stages
+ckd15 = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-ckd15.csv",
+  system="snomed",
+  column="code",
+)
+
+# Chronic kidney disease codes-stages 3 - 5
+ckd35 = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-ckd35.csv",
+  system="snomed",
+  column="code",
+)
+
+# Chronic Liver disease codes
+cld = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-cld.csv",
+  system="snomed",
+  column="code",
+)
+
+# Diabetes diagnosis codes
+diab = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-diab.csv",
+  system="snomed",
+  column="code",
+)
+
+# Immunosuppression diagnosis codes
+immdx_cov = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-immdx_cov.csv",
+  system="snomed",
+  column="code",
+)
+
+# Immunosuppression medication codes
+immrx = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-immrx.csv",
+  system="snomed",
+  column="code",
+)
+
+# Chronic Neurological Disease including Significant Learning Disorder
+cns_cov = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-cns_cov.csv",
+  system="snomed",
+  column="code",
+)
+
+# Asplenia or Dysfunction of the Spleen codes
+spln_cov = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-spln_cov.csv",
+  system="snomed",
+  column="code",
+)
+
+# BMI
+bmi = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-bmi.csv",
+  system="snomed",
+  column="code",
+)
+
+# All BMI coded terms
+bmi_stage = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-bmi_stage.csv",
+  system="snomed",
+  column="code",
+)
+
+# Severe Obesity code recorded
+sev_obesity = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-sev_obesity.csv",
+  system="snomed",
+  column="code",
+)
+
+# Diabetes resolved codes
+dmres = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-dmres.csv",
+  system="snomed",
+  column="code",
+)
+
+# Severe Mental Illness codes
+sev_mental = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-sev_mental.csv",
+  system="snomed",
+  column="code",
+)
+
+# Remission codes relating to Severe Mental Illness
+smhres = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-smhres.csv",
+  system="snomed",
+  column="code",
+)
+
+
+# to represent household contact of shielding individual
+hhld_imdef = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-hhld_imdef.csv",
+  system="snomed",
+  column="code",
+)
+
+# Wider Learning Disability
+learndis = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-learndis.csv",
+  system="snomed",
+  column="code",
+)
+
+# Carer codes
+carer = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-carer.csv",
+  system="snomed",
+  column="code",
+)
+
+# No longer a carer codes
+notcarer = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-notcarer.csv",
+  system="snomed",
+  column="code",
+)
+
+# Employed by Care Home codes
+carehomeemployee = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-carehome.csv",
+  system="snomed",
+  column="code",
+)
+
+# Employed by nursing home codes
+nursehomeemployee = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-nursehome.csv",
+  system="snomed",
+  column="code",
+)
+
+# Employed by domiciliary care provider codes
+domcareemployee = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-domcare.csv",
+  system="snomed",
+  column="code",
+)
+
+# end of life care
+eol = codelist_from_csv(
+    "codelists/nhsd-primary-care-domain-refsets-palcare_cod.csv",
+    system="snomed",
+    column="code",
+)
+
+midazolam = codelist_from_csv(
+    "codelists/opensafely-midazolam-end-of-life.csv",
+    system="snomed",
+    column="dmd_id",   
+)
+
+housebound = codelist_from_csv(
+    "codelists/opensafely-housebound.csv", 
+    system="snomed", 
+    column="code"
+)
+
+no_longer_housebound = codelist_from_csv(
+    "codelists/opensafely-no-longer-housebound.csv", 
+    system="snomed", 
+    column="code"
+)
+
+discharged_to_hospital = codelist(
+    ["306706006", "1066331000000109", "1066391000000105"],
+    system="snomed",
+)
+
+# flu vaccine codes
+flu_med_codes = codelist_from_csv(
+    "codelists/opensafely-influenza-vaccination.csv",  
+    system="snomed",  
+    column="snomed_id",
+)
+flu_clinical_given_codes = codelist_from_csv(
+    "codelists/opensafely-influenza-vaccination-clinical-codes-given.csv",  
+    system="ctv3", 
+    column="CTV3ID",
+)
+flu_clinical_not_given_codes = codelist_from_csv(
+    "codelists/opensafely-influenza-vaccination-clinical-codes-not-given.csv",  
+    system="ctv3", 
+    column="CTV3ID",
+)
+
+fractures_icd10 = codelist_from_csv(
+    "codelists/opensafely-fractures.csv",
+    system="icd10",
+    column="code"
+)
+
+fractures_snomedECDS = codelist(
+  [
+    "371162008","81639003","430984009","66112004","60667009","269062008","207938004",
+    "207957008","13695006","207974008","766775007","91037003","33173003","29749002",
+    "43295006","302222008","111640008","71555008","53627009","29045004","208322000",
+    "208371005","9468002","208394006","208403005","704213001","24424003","359817006",
+    "25415003","428151000","80756009","447139008","447395005","413877007","42188001",
+    "64665009","342070009","81576005","371161001","111609001","40613008","87225004",
+    "45910007","269070003","207949005","207965006","207977001","767262002","15474008",
+    "111637008","47864008","89294002","302232001","42945005","37449000","81966000",
+    "34578006","208341002","29014003","1370007","208420009","704236005","21698002",
+    "361118003","28576007","428019004","111643005","446979005","447017008","414943006",
+    "481870042","4948002","367527001", "74395007", "95851007"
+  ],
+  system="snomed"
+)
+
+# for defining deaths due to cardiovascvular disease
+
+# Acute myocardial infarction
+ami_icd10 = codelist_from_csv(
+    "codelists/user-RochelleKnight-ami_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Ischaemic stroke
+stroke_isch_icd10 = codelist_from_csv(
+    "codelists/user-RochelleKnight-stroke_isch_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Deep vein thrombosis
+dvt_dvt_icd10 = codelist_from_csv(
+    "codelists/user-RochelleKnight-dvt_dvt_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Deep vein thrombosis in pregnancy
+dvt_pregnancy_icd10 = codelist_from_csv(
+    "codelists/user-elsie_horne-dvt_pregnancy_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Pulmonary embolism
+pe_icd10 = codelist_from_csv(
+    "codelists/user-RochelleKnight-pe_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Transient ischaemic attack
+tia_icd10 = codelist_from_csv(
+    "codelists/user-RochelleKnight-tia_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Subarachnoid haemorrhage and haemorrhagic stroke
+stroke_sah_hs_icd10 = codelist_from_csv(
+    "codelists/user-RochelleKnight-stroke_sah_hs_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Ischaemic stroke
+stroke_isch_icd10 = codelist_from_csv(
+    "codelists/user-RochelleKnight-stroke_isch_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Heart failure
+hf_icd10 = codelist_from_csv(
+    "codelists/user-RochelleKnight-hf_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Angina
+angina_icd10 = codelist_from_csv(
+    "codelists/user-RochelleKnight-angina_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Other arterial embolism
+other_arterial_embolism_icd10 = codelist_from_csv(
+    "codelists/user-elsie_horne-other_arterial_embolism_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Portal vein thrombosis
+portal_vein_thrombosis_icd10 = codelist_from_csv(
+    "codelists/user-elsie_horne-portal_vein_thrombosis_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Cerebral vein thrombosis
+dvt_icvt_icd10 = codelist_from_csv(
+    "codelists/user-elsie_horne-dvt_icvt_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Other deep vein thrombosis
+other_dvt_icd10 = codelist_from_csv(
+    "codelists/user-elsie_horne-other_dvt_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Cerebral vein thrombosis in pregnancy
+icvt_pregnancy_icd10 = codelist_from_csv(
+    "codelists/user-elsie_horne-icvt_pregnancy_icd10.csv",
+    system="icd10",
+    column="code",
+)
+
+cvd_combined = combine_codelists(
+    # Acute myocardial infarction
+    ami_icd10,
+    # Ischaemic stroke
+    stroke_isch_icd10,
+    # Deep vein thrombosis (all_dvt_codes_icd10)
+    dvt_dvt_icd10,
+    dvt_pregnancy_icd10,
+    # Pulmonary embolism
+    pe_icd10,
+    # Transient ischaemic attack
+    tia_icd10,
+    # Subarachnoid haemorrhage and haemorrhagic stroke
+    stroke_sah_hs_icd10,
+    # Heart failure
+    hf_icd10,
+    # Angina
+    angina_icd10,
+    # Other arterial embolism
+    other_arterial_embolism_icd10,
+    # Venous thromboembolism events (all_vte_codes_icd10)
+    # Portal vein thrombosis
+    portal_vein_thrombosis_icd10, 
+    # Cerebral vein thrombosis
+    dvt_icvt_icd10, 
+    # Other deep vein thrombosis
+    other_dvt_icd10, 
+    # Cerebral vein thrombosis in pregnancy
+    icvt_pregnancy_icd10, 
+)
+
+# cancer (any malignant neoplasm icd 10 code)
+cancer = codelist_from_csv(
+    "codelists/user-elsie_horne-cancer_icd10.csv",
+    system="icd10",
+    column="code",
+)
+
+# cancer snomed codes
+cancer_haem_snomed=codelist_from_csv(
+    "codelists/opensafely-haematological-cancer-snomed.csv",
+    system="snomed",
+    column="id",
+)
+
+cancer_nonhaem_nonlung_snomed=codelist_from_csv(
+    "codelists/opensafely-cancer-excluding-lung-and-haematological-snomed.csv",
+    system="snomed",
+    column="id",
+)
+
+cancer_lung_snomed=codelist_from_csv(
+    "codelists/opensafely-lung-cancer-snomed.csv",
+    system="snomed",
+    column="id",
+)

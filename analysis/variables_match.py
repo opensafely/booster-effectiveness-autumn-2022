1970d0d07523f9cf199dd72a7878ce605761be4c
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2023-07-11 08:36:20 +0100

    Update variables_match.py

--- analysis/variables_match.py
+++ analysis/variables_match.py
@@ -4,32 +4,36 @@ import codelists
 
 
 def generate_match_variables(
-    index_date, 
+    index_date,
+    # extract variables for assessing eligibility by default,
+    # but don't do this when extracting covariates 
+    elig = True,
     # default to everything, so that when extracting 
     # treated and potential for matchround1
     # all potiential matching variables extracted
-    match_vars = "everything", 
+    vars = "everything", 
     ):
   
-  ############################################################
-  ## elig variables
-  from variables_elig import generate_elig_variables 
-  elig_variables = generate_elig_variables(index_date=index_date)
-  ############################################################
-  ## jcvi variables
-  from variables_jcvi import generate_jcvi_variables 
-  jcvi_variables = generate_jcvi_variables(index_date=index_date)
-  ############################################################
-  
-  # always extract these variables
-  variables = dict(
-    # variables that are used in assessing prematch eligibility    
-    **elig_variables,
-    **jcvi_variables,
-  )
+  # empty dict for variables
+  variables = dict()
 
-  # only extract the following variables when needed in matching
-  if any(x in match_vars for x in {'everything', 'region'}):
+  if elig:
+    # elig variables
+    from variables_elig import generate_elig_variables 
+    elig_variables = generate_elig_variables(index_date=index_date)
+    # jcvi variables
+    from variables_jcvi import generate_jcvi_variables 
+    jcvi_variables = generate_jcvi_variables(index_date=index_date)
+    # extract variables that are used in assessing prematch eligibility    
+    variables.update(
+      **elig_variables,
+      **jcvi_variables,
+    )
+
+  # only extract the following variables if specified:
+
+  # region
+  if any(x in vars for x in {'everything', 'region'}):
     variables.update(
         
         # NHS administrative region
@@ -56,5 +60,33 @@ def generate_match_variables(
         ),
 
     )
+
+    if any(x in vars for x in {'everything', 'flu_vaccine'}):
+      # flu vaccine in flu seasons 18-19, 19-20 or 20-21
+      flu_vaccine=patients.satisfying(
+          """
+          flu_vaccine_tpp_table>0 OR
+          flu_vaccine_med>0 OR
+          flu_vaccine_clinical>0
+          """,
+          flu_vaccine_tpp_table=patients.with_tpp_vaccination_record(
+              target_disease_matches="INFLUENZA",
+              between=["2018-07-01", "2021-06-30"], 
+              returning="binary_flag",
+          ),
+          flu_vaccine_med=patients.with_these_medications(
+              codelists.flu_med_codes,
+              between=["2018-07-01", "2021-06-30"], 
+              returning="binary_flag",
+          ),
+          flu_vaccine_clinical=patients.with_these_clinical_events(
+              codelists.flu_clinical_given_codes,
+              ignore_days_where_these_codes_occur=codelists.flu_clinical_not_given_codes,
+              between=["2018-07-01", "2021-06-30"], 
+              returning="binary_flag",
+          ),
+          return_expectations={"incidence": 0.5, },
+      ),
+
 
     return variables

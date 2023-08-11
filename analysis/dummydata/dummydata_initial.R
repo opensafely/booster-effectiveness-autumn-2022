099ce16d53dad17cb164ba7c25671ec4bfeb3708
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2023-08-11 16:59:25 +0100

    Update dummydata_initial.R

--- analysis/dummydata/dummydata_initial.R
+++ analysis/dummydata/dummydata_initial.R
@@ -24,79 +24,96 @@ if(Sys.getenv("OPENSAFELY_BACKEND") %in% c("", "expectations")){
   )
   
   # create dummy data with realistic vaccination dates
-  data_vax_wide <- tibble(
-    
-    age = as.integer(runif(population_size, 50,90)),
-    
-    # primary course (recorded for everyone)
-    vax_dose1_day = runif(
-      n = population_size, 
-      study_days$dose1$pfizer, 
-      study_days$dose1$pfizer + 120
-    ),
-    vax_dose1_brand = sample(
-      x = c("pfizer", "az", "moderna"), 
-      size = population_size, 
-      replace = TRUE, 
-      prob = c(0.45,0.45,0.1)
-    ),
-    vax_dose2_day = runif(
-      n = population_size, 
-      vax_dose1_day + 70, 
-      vax_dose1_day + 100
-    ),
-    vax_dose2_brand = vax_dose1_brand,
-    
-    # first booster
-    boosterfirst = rbern(population_size, p = 0.9),
-    vax_boosterfirst_day = runif(
-      n = population_size,
-      study_days$boosterfirst$pfizerstart,
-      study_days$boosterfirst$end
-    ),
-    vax_boosterfirst_brand = sample(
-      x = c("pfizer", "az", "moderna"), 
-      size = population_size, 
-      replace = TRUE, 
-      prob = c(0.45,0.1,0.45)
-    ),
-    
-    # spring booster
-    boosterspring = (age >= 75) & rbern(population_size, 0.8),
-    vax_boosterspring_day = runif(
-      n = population_size, 
-      study_days$boosterspring$start, 
-      study_days$boosterspring$end
-    ),
-    vax_boosterspring_brand = sample(
-      x = c("pfizer", "moderna"), 
-      size = population_size, 
-      replace = TRUE, 
-      prob = c(0.5,0.5)
-    ),
-    
-    # autumn booster
-    boosterautumn = rbern(population_size, 0.5),
-    vax_boosterautumn_day = runif(
-      n = population_size, 
-      study_days$boosterautumn$ages65plus, 
-      study_days$boosterautumn$ages65plus + 90
+  data_vax_wide <- tibble(patient_id = 1:population_size) %>%
+    mutate(
+      
+      age = as.integer(runif(population_size, 18,120)),
+      
+      # dose1
+      tmp_dose1 = rbern(population_size, p = 0.9),
+      vax_dose1_day = runif(
+        n = population_size, 
+        study_days$dose1$pfizer - 7, # build in some errors to check code 
+        study_days$dose1$pfizer + 6*7
       ),
-    vax_boosterautumn_brand = sample(
-      x = c("pfizerbivalent", "modernabivalent"), 
-      size = population_size, 
-      replace = TRUE, 
-      prob = c(0.5,0.5)
-    )
-    
-  ) %>%
-    mutate(across(vax_boosterfirst_day, ~if_else(boosterfirst, .x, NA_real_))) %>%
-    mutate(across(vax_boosterspring_day, ~if_else(boosterspring, .x, NA_real_))) %>%
-    mutate(across(vax_boosterautumn_day, ~if_else(boosterautumn, .x, NA_real_))) %>%
+      vax_dose1_brand = sample(
+        x = c("pfizer", "az", "moderna"), 
+        size = population_size, 
+        replace = TRUE, 
+        prob = c(0.45,0.45,0.1)
+      ),
+      
+      # dose 2
+      tmp_dose2 = tmp_dose1 & rbern(population_size, p = 0.9),
+      tmp_primarycourse_heterologous = rbern(population_size, p = 0.05),
+      vax_dose2_day = runif(
+        n = population_size, 
+        vax_dose1_day, 
+        vax_dose1_day + 14*7
+      ),
+      vax_dose2_brand = sample(
+        x = c("pfizer", "az", "moderna"), 
+        size = population_size, 
+        replace = TRUE, 
+        prob = c(0.45,0.45,0.1)
+      ),
+      vax_dose2_brand = if_else(
+        tmp_primarycourse_heterologous, 
+        vax_dose2_brand, 
+        vax_dose1_brand
+        ),
+      
+      # first booster
+      tmp_boosterfirst = tmp_dose2 & rbern(population_size, p = 0.9),
+      vax_boosterfirst_day = runif(
+        n = population_size,
+        pmax(vax_dose2_day, study_days$boosterfirst$pfizerstart - 7),
+        pmax(vax_dose2_day, study_days$boosterfirst$end) + 7
+      ),
+      vax_boosterfirst_brand = sample(
+        x = c("pfizer", "az", "moderna"), 
+        size = population_size, 
+        replace = TRUE, 
+        prob = c(0.45,0.1,0.45)
+      ),
+      
+      # spring booster
+      tmp_boosterspring = (age >= 75) & rbern(population_size, 0.8),
+      vax_boosterspring_day = runif(
+        n = population_size, 
+        pmax(vax_boosterfirst_day, study_days$boosterspring$start - 7), 
+        pmax(vax_boosterfirst_day, study_days$boosterspring$end) + 7
+      ),
+      vax_boosterspring_brand = sample(
+        x = c("pfizer", "moderna"), 
+        size = population_size, 
+        replace = TRUE, 
+        prob = c(0.5,0.5)
+      ),
+      
+      # autumn booster
+      tmp_boosterautumn = (age >= 50) & rbern(population_size, 0.5),
+      vax_boosterautumn_day = runif(
+        n = population_size, 
+        pmax(vax_boosterspring_day, study_days$boosterautumn$ages65plus - 7), 
+        pmax(vax_boosterspring_day, study_days$boosterautumn$ages65plus) + 7*12
+      ),
+      vax_boosterautumn_brand = sample(
+        x = c("pfizerbivalent", "modernabivalent"), 
+        size = population_size, 
+        replace = TRUE, 
+        prob = c(0.5,0.5)
+      )
+      
+    ) %>%
+    mutate(across(vax_dose1_day, ~if_else(tmp_dose1, .x, NA_real_))) %>%
+    mutate(across(vax_dose2_day, ~if_else(tmp_dose2, .x, NA_real_))) %>%
+    mutate(across(vax_boosterfirst_day, ~if_else(tmp_boosterfirst, .x, NA_real_))) %>%
+    mutate(across(vax_boosterspring_day, ~if_else(tmp_boosterspring, .x, NA_real_))) %>%
+    mutate(across(vax_boosterautumn_day, ~if_else(tmp_boosterautumn, .x, NA_real_))) %>%
     mutate(across(ends_with("day"), ~ study_dates$studystart + as.integer(.x))) %>%
     rename_with(~str_replace(.x, "day", "date")) %>%
-    select(-c(boosterfirst, boosterspring, boosterautumn)) %>%
-    mutate(patient_id = row_number(), .before = 1) 
+    select(-starts_with("tmp_")) 
   
   # process dummy data to have same format as study definition
   data_vax_long <- data_vax_wide %>%
@@ -105,18 +122,11 @@ if(Sys.getenv("OPENSAFELY_BACKEND") %in% c("", "expectations")){
       names_pattern = "vax_(.*)_(.*)",
       names_to = c("dose", ".value")
     ) %>%
-    mutate(across(brand, ~if_else(is.na(date), NA_character_, .x))) %>%
     filter(!is.na(date)) %>%
     select(patient_id, age, date, brand) %>%
     distinct(patient_id, age, date, .keep_all = TRUE) 
   
   data_vax_disease <- data_vax_long %>%
-    # add an extra random vax date to similate errors in the real data
-    bind_rows(
-      data_vax_long %>%
-        slice_sample(n = 0.05*population_size) %>%
-        mutate(across(date, ~ .x + as.integer(rnorm(n = 0.05*population_size, sd = 5))))
-    ) %>%
     select(-brand) %>%
     group_by(patient_id) %>%
     arrange(date, .by_group = TRUE) %>%
@@ -153,6 +163,8 @@ if(Sys.getenv("OPENSAFELY_BACKEND") %in% c("", "expectations")){
   data_initial <- bind_rows(data_initial, data_initial) %>%
     # define static variables
     mutate(
+      registered = rbern(nrow(.), p = 0.99),
+      has_died = rbern(nrow(.), p = 0.01),
       sex = sample(x = c("M", "F"), size = nrow(.), replace = TRUE),
       ethnicity = sample(
         x = c("White", "Mixed", "Asian or Asian British", "Black or Black British", "Other", "Unknown"),
